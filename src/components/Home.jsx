0eb616e0a38a056f233c32e044cb64000535b317
shivamgehani <shivam.bscsses21@iba-suk.edu.pk>
2024-07-07 00:36:03 +0500

    Update site with latest changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useRef } from "react";
-
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
 const Home = () => {
   const typedRef = useRef(null);
+
   useEffect(() => {
     const options = {
       strings: [
-        "Welme to my profile",
+        "Welcome to my profile",
         "My Name is Shivam Gehani",
         "I'm full stack developer",
-        "Androide Developer (React Native)",
+        "Android Developer (React Native)",
       ],
       typeSpeed: 50,
       backSpeed: 50,
@@ -26,24 +26,22 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <div className="container home" id="home">
-        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
-          <h1 ref={typedRef}></h1>
-          
-          <a href="https://drive.google.com/file/d/1fReF94jlTRFSw9u82P-kohZHGAHulUBZ/view?usp=sharing"  className="btn btn-outline-warning my-3">
-                     Resume 
-                    </a>
-
-         
-        </div>
-        <div className="right">
-          <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
-            <img src={`/assets/${hero.imgSrc}`} alt="hero" />
-          </div>
+    <div className="container home" id="home">
+      <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+        <h1 ref={typedRef}></h1>
+        <a
+          href="https://drive.google.com/file/d/1fReF94jlTRFSw9u82P-kohZHGAHulUBZ/view?usp=sharing"
+          className="btn btn-outline-warning my-3"
+        >
+          Resume
+        </a>
+      </div>
+      <div className="right">
+        <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+          <img src={`/assets/${hero.imgSrc}`} alt="hero" />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
